8acec04b164b86df22e3853b698e74f464f86d8a
Hidemi Ishihara <hidemi@aquaxis.com>
2019-12-10 17:07:30 +0900

    modify signal

--- src/debug_top.v
+++ src/debug_top.v
@@ -1,157 +1,123 @@
 module debug_top(
-    input TRST_N,
-    input TCK,
-    input TMS,
-    input TDI,
+    input  TRST_N,
+    input  TCK,
+    input  TMS,
+    input  TDI,
     output TDO,
 
-    output test_logic_reset_o,
-    output run_test_idle_o,
-    output shift_dr_o,
-    output pause_dr_o,
-    output update_dr_o,
-    output capture_dr_o,
-
-    output extest_select_o,
-    output sample_preload_select_o,
-    output mbist_select_o,
-    output debug_select_o,
-    output dtmcs_select_o,
-    output dmi_select_o,
-
-    output dmi_wr,
-    output dmi_rd,
-
-    output tdi_o,
-
-    input debug_tdo_i,
-    input bs_chain_tdo_i,
-    input mbist_tdo_i,
-
-    input CLK100MHZ,
+    input  CLK100MHZ,
 
     output [3:0] LED
 );
 
-wire tdo_o, tdo_oe;
+  wire tdo_o, tdo_oe;
 
-assign TDO = (tdo_oe)?tdo_o:1'bz;
+  assign TDO = (tdo_oe)?tdo_o:1'bz;
 
-wire [3:0] dmuuy;
+  wire        dmi_en;
+  wire        dmi_wr, dmi_rd;
+  wire [6:0]  dmi_ad;
+  wire [31:0] dmi_di, dmi_do;
 
-wire dmi_select_o;
-wire dmi_wr, dmi_rd;
-wire [6:0] dmi_ad;
-wire [31:0] dmi_di, dmi_do;
+  wire        ar_en;
+  wire        ar_wr;
+  wire [15:0] ar_ad;
+  wire [31:0] ar_di, ar_do;
 
-wire ar_en;
-wire ar_wr;
-wire [15:0] ar_ad;
-wire [31:0] ar_di, ar_do;
+  wire        am_en;
+  wire        am_wr;
+  wire [3:0]  am_st;
+  wire [31:0] am_ad;
+  wire [31:0] am_di, am_do;
 
-wire core_haltreq, core_resumereq;
-reg core_halt, core_resume;
+  wire        sys_en, sys_wr;
+  wire [31:0] sys_ad, sys_di, sys_do;
 
-debug_dtm u_debug_dtm(
+  wire        core_reset;
+  wire        core_haltreq, core_resumereq;
+  reg         core_halt, core_resume;
+
+  wire        ndmreset;
+
+  debug_dtm u_debug_dtm(
     // JTAG pads
-    .tms_pad_i      (TMS),
-    .tck_pad_i      (TCK),
-    .trstn_pad_i    (TRST_N),
-    .tdi_pad_i      (TDI),
-    .tdo_pad_o      (tdo_o),
-    .tdo_padoe_o    (tdo_oe),
+    .TMS      ( TMS    ),
+    .TCK      ( TCK    ),
+    .TRSTN    ( TRST_N ),
+    .TDI      ( TDI    ),
+    .TDO      ( tdo_o  ),
+    .TDO_OE   ( tdo_oe ),
 
-    // TAP states
-    .test_logic_reset_o (test_logic_reset_o),
-    .run_test_idle_o    (run_test_idle_o),
-    .shift_dr_o         (shift_dr_o),
-    .pause_dr_o         (pause_dr_o),
-    .update_dr_o        (update_dr_o),
-    .capture_dr_o       (capture_dr_o),
+    .TDI_O    ( tdi_o  ),
 
-    // Select signals for boundary scan or mbist
-    .extest_select_o        (extest_select_o),
-    .sample_preload_select_o(sample_preload_select_o),
-    .mbist_select_o         (mbist_select_o),
-    .debug_select_o         (debug_select_o),
-    .dtmcs_select_o         (dtmcs_select_o),
-    .dmi_select_o           (dmi_select_o),
+    .DMI_EN   ( dmi_en ),
+    .DMI_WR   ( dmi_wr ),
+    .DMI_RD   ( dmi_rd ),
+    .DMI_AD   ( dmi_ad ),
+    .DMI_DI   ( dmi_di ),
+    .DMI_DO   ( dmi_do )
+  );
 
-    // TDO signal that is connected to TDI of sub-modules.
-    .tdi_o                  (tdi_o),
+  debug_dm u_debug_dm(
+    .RST_N    ( TRST_N ),
+    .CLK      ( TCK    ),
 
-    .dmi_wr                 (dmi_wr),
-    .dmi_rd                 (dmi_rd),
-    .dmi_ad                 (dmi_ad),
-    .dmi_di                 (dmi_di),
-    .dmi_do                 (dmi_do),
+    // DMI
+    .DMI_CS   ( dmi_en ),
+    .DMI_WR   ( dmi_wr ),
+    .DMI_RD   ( dmi_rd ),
+    .DMI_AD   ( dmi_ad ),
+    .DMI_DI   ( dmi_do ),
+    .DMI_DO   ( dmi_di ),
 
-    // TDI signals from sub-modules
-    .debug_tdo_i    (debug_tdo_i),    // from debug module
-    .bs_chain_tdo_i (bs_chain_tdo_i), // from Boundary Scan Chain
-    .mbist_tdo_i    (mbist_tdo_i)     // from Mbist Chain
-);
+    // Debug Module Status
+    .I_IMPEBREAK        (),
+    .I_HAVERESET        ( core_reset     ),
+    .I_RESUMEACK        ( core_resume    ),
+    .I_NONEXISTENT      (),
+    .I_UNAVAIL          (),
+    .I_RUNNING          (),
+    .I_HALTED           ( core_halt      ),
+    .I_AUTHENTICATED    (),
+    .I_AUTHBUSY         (),
+    .I_HASRESETHALTREQ  (),
+    .I_CONFSTRPTRVALID  (),
 
-debug_dm u_debug_dm(
-  .RST_N(TRST_N),
-  .CLK(TCK),
+    .O_HALTREQ          ( core_haltreq   ),
+    .O_RESUMEREQ        ( core_resumereq ),
+    .O_HARTRESET        ( core_reset     ),
+    .O_ACKHAVERESET     (),
+    .O_SETRESETHALTREQ  (),
+    .O_CLRRESETHALTREQ  (),
+    .O_NDMRESET         ( ndmreset       ),
 
-  // DMI
-  .DMI_CS(dmi_select_o),
-  .DMI_WR(dmi_wr),
-  .DMI_RD(dmi_rd),
-  .DMI_AD(dmi_ad),
-  .DMI_DI(dmi_do),
-  .DMI_DO(dmi_di),
+    .AR_EN  ( ar_en),
+    .AR_WR  ( ar_wr),
+    .AR_AD  ( ar_ad),
+    .AR_DI  ( ar_di),
+    .AR_DO  ( ar_do),
 
-  // Debug Module Status
-  .I_IMPEBREAK(),
-  .I_HAVERESET(),
-  .I_RESUMEACK(core_resume),
-  .I_NONEXISTENT(),
-  .I_UNAVAIL(),
-  .I_RUNNING(),
-  .I_HALTED(core_halt),
-  .I_AUTHENTICATED(),
-  .I_AUTHBUSY(),
-  .I_HASRESETHALTREQ(),
-  .I_CONFSTRPTRVALID(),
+    .AM_EN  ( am_en),
+    .AM_WR  ( am_wr),
+    .AM_ST  ( am_st),
+    .AM_AD  ( am_ad),
+    .AM_DI  ( am_di),
+    .AM_DO  ( am_do),
 
-  .O_HALTREQ(core_haltreq),
-  .O_RESUMEREQ(core_resumereq),
-  .O_HARTRESE(),
-  .O_ACKHAVERESE(),
-  .O_SERESEHALTREQ(),
-  .O_CLRRESETHALTREQ(),
-  .O_NDMRESE(),
+    .SYS_EN ( sys_en),
+    .SYS_WR ( sys_wr),
+    .SYS_AD ( sys_ad),
+    .SYS_DI ( sys_di),
+    .SYS_DO ( sys_do),
 
-  .AR_EN(ar_en),
-  .AR_WR(ar_wr),
-  .AR_AD(ar_ad),
-  .AR_DI(ar_di),
-  .AR_DO(ar_do),
-
-  .DEBUG()
-);
-
-assign LED[0] = 4'd0;
-
-ila_0 u_ila_0(
-    .clk(CLK100MHZ),
-    .probe0(dmi_select_o),
-    .probe1(dmi_wr),
-    .probe2(dmi_rd),
-    .probe3(dmi_ad),
-    .probe4(dmi_di),
-    .probe5(dmi_do),
-    .probe6(TCK)
-);
+    .DEBUG  ()
+  );
 
 reg [31:0] data;
 always @(*) begin
     case(ar_ad)
-    16'h0301: data <= 32'h4000_1105;
-    default: data <= 32'd0;
+    16'h0301: data <= 32'h4000_1105;    // 
+    default:  data <= 32'd0;
     endcase
 end
 
@@ -174,5 +140,10 @@ always @(posedge CLK100MHZ or negedge TRST_N) begin
         end
     end
 end
+
+assign LED[0] = core_resume;
+assign LED[1] = core_halt;
+assign LED[2] = 1'b0;
+assign LED[3] = 1'b0;
 
 endmodule
